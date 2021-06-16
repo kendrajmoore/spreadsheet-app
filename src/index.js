5972d9a38a1cddd840a0eb6816e7393cb3832133
kendrajmoore <kendra.j.moore3443@gmail.com>
2021-06-15 22:28:52 -0400

    add routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+
+import { stopReportingRuntimeErrors } from "react-error-overlay";
+
+
+if (process.env.NODE_ENV === "development") {
+  stopReportingRuntimeErrors(); // disables error overlays
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
 
